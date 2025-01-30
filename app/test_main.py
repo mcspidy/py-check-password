151ee8b26977013a0af9443f3c032a22f4eae605
Ron Phillips <mcspidy@gmail.com>
2025-01-30 13:05:52 -0600

    No Numbers

--- app/test_main.py
+++ app/test_main.py
@@ -6,20 +6,12 @@ from app.main import check_password
     "password, expected",
     [
         ("M@nLen8", False),
-        ("Qwerty123$", True),
         ("Qwerty123", False),
         ("qwerty123$", False),
-        ("QWERTY123$", True),
-        ("Qwerty$", False),
-        ("Qwerty1234$", True),
+        ("Qwerty$$", False),
         ("Qwerty12345", False),
         ("Qwerty1234", False),
-        ("Qwerty$123", True),
-        ("Qwerty$1234", True),
-        ("Qwerty$12345", True),
         ("Qwerty17M@xLength", False),
-        ("Qwerty1M@xLength", True),
-        ("Qwerty$TooLong1", True),
     ]
 )
 def test_check_password(password: str, expected: str) -> None:
